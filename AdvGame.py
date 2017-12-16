5201f819e9b9f4ec0ebfc20703981ed4f3940b3e
Taylor Bryant <taylorbryant@TaylorBntsiMac2.attlocal.net>
2017-12-16 10:46:59 -0600

    Worked on inventory

--- AdvGame.py
+++ AdvGame.py
@@ -4,14 +4,17 @@
 #Date: 9/22/17
 #This is a simple text based game created in the language of python
 
+#-----------------------IMPORTS-----------------------------
 from collections import namedtuple
 
-Item = namedtuple('Item', ('attack', 'defence', 'price', 'Type'))
+Item = namedtuple('Item', ('attack', 'defence', 'price', 'Type', 'Pclass'))
 Items = {
-    'sword': Item(10,0,10,'weapon'),
-    'wand': Item(5,0,2,'weapon'),
-    'dagger': Item(7,0,5,'weapon')
+    'sword': Item(10,0,10,'weapon','swordsman'),
+    'wand': Item(5,0,2,'weapon', 'wizard'),
+    'dagger': Item(7,0,5,'weapon', 'rouge')
 }
+
+#------------------------CLASSES-----------------------------
 
 #Player stats
 class Player:
@@ -20,36 +23,27 @@ class Player:
     hp = 0
     Strength = 0
     Defense = 0
+    gold = 0
     lvl = 1
+    Pclass = ""
     Inventory = {}
     Equipment = dict()
 
+#-----------------------FUNCTIONS-----------------------------
+
 #Inserting Item into equipment
-def EquipInsert(item_name): 
+def EquipInsert(item_name):
+    ItemsIndex = 0
     for key, value in list(Items.items()):
         if key == item_name:
-            itemType = value.Type
-    
-    if bool(Player.Equipment) != False:
-        length = len(Player.Equipment) - 1
-        for key in Player.Equipment[length]:
-            if key != itemType:
-                print("Hello")
-            if length != 0:
-                length -= 1
-                
-    ItemsIndex = 0
-    for key in Items:
-        if key == item_name:
-            Player.Equipment = list(Items.items())[ItemsIndex]
+            itemClass = value.Pclass
+            Index = ItemsIndex
         ItemsIndex += 1
-#    elif bool(Player.Equipment) == False:
-#        ItemsIndex = 0
-#        for key in Items:
-#            if key == item_name:
-#                Player.Equipment = list(Items.items())[ItemsIndex]
-#            ItemsIndex += 1
-
+    
+    for key in Items:
+        if key == item_name and Player.Pclass == itemClass:
+            Player.Equipment = list(Items.items())[Index]
+            ItemsIndex += 1
         
 #Game Intro
 def GameIntro():
@@ -65,16 +59,17 @@ def GameIntro():
     #Choosing a class
     if choice == "1":
         print("Welcome to the swordsman class\n")
+        Player.Pclass = 'swordsman'
         Player.Strength = 10
         Player.Defense = 10
         Player.hp = 100
         Player.MaxHP = 100
         EquipInsert('sword')
         EquipInsert('wand')
-        print(Player.Equipment)
         print("You have been given a " + Player.Equipment[0] + "\n")
     elif choice == "2":
         print("Welcome to the wizard class\n")
+        Player.Pclass = 'wizard'
         Player.Strength = 5
         Player.Defense = 3
         Player.hp = 50
@@ -82,8 +77,8 @@ def GameIntro():
         EquipInsert('wand')
         print("You have been given a " + Player.Equipment[0] + "\n")
     elif choice == "3":
-        print(Items)
         print("Welcome to the rogue class\n")
+        Player.Pclass = 'rouge'
         Player.Strength = 7
         Player.Defense = 5
         Player.hp = 70
@@ -126,7 +121,7 @@ def Choices():
     
 #Main game function
 def main():
-    GameIntro()  
+    GameIntro()
     Choices()
 
 if __name__ == '__main__':
