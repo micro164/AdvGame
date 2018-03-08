42f6b353aacaba4098d7ec794a8066fcfe0bf276
micro164 <micro164@gmail.com>
2018-03-07 18:53:19 -0600

    Added saving to the game, although it does not yet save the equipment and inventory at this point.

--- AdvGame.py
+++ AdvGame.py
@@ -201,56 +201,61 @@ def NameCheck(name):
 
 #Game Intro
 def GameIntro():
-    print("Welcome to TxtBasedAdv")
-    print("Please enter a name: ")
-    Player.name = input()
-    NameCheck(Player.name)
-    print("Hello " + Player.name)
+    if Player.Pclass == "\"\"\n":
+        print("Welcome to TxtBasedAdv")
+        print("Please enter a name: ")
+        Player.name = input()
+        NameCheck(Player.name)
+        print("Hello " + Player.name)
 
-    print("\nChoose your class")
-    print("1.Swordsman Class \n2.Wizard Class \n3.Rogue Class")
-    choice = input()
+        print("\nChoose your class")
+        print("1.Swordsman Class \n2.Wizard Class \n3.Rogue Class")
+        choice = input()
 
-    #Choosing a class
-    if choice == "1":
-        print("Welcome to the swordsman class\n")
-        Player.Pclass = 'swordsman'
-        Player.Strength = 10
-        Player.Defense = 10
-        Player.hp = 100
-        Player.MaxHP = 100
-        EquipInsert('sword')
-        print("You have been given a sword.")
-        EquipInsert('bronze armor')
-        print("You have been given a bronze armor\n")
-        Intro()
-    elif choice == "2":
-        print("Welcome to the wizard class\n")
-        Player.Pclass = 'wizard'
-        Player.Strength = 5
-        Player.Defense = 3
-        Player.hp = 50
-        Player.MaxHP = 50
-        EquipInsert('wand')
-        print("You have been given a wand.")
-        EquipInsert('cloth armor')
-        print("You have been given a cloth armor\n")
-        Intro()
-    elif choice == "3":
-        print("Welcome to the rogue class\n")
-        Player.Pclass = 'rouge'
-        Player.Strength = 7
-        Player.Defense = 5
-        Player.hp = 70
-        Player.MaxHP = 70
-        EquipInsert('dagger')
-        print("You have been given a dagger.")
-        EquipInsert('hard cloth armor')
-        print("You have been given a hard cloth armor.\n")
+        #Choosing a class
+        if choice == "1":
+            print("Welcome to the swordsman class\n")
+            Player.Pclass = 'swordsman'
+            Player.Strength = 10
+            Player.Defense = 10
+            Player.hp = 100
+            Player.MaxHP = 100
+            EquipInsert('sword')
+            print("You have been given a sword.")
+            EquipInsert('bronze armor')
+            print("You have been given a bronze armor\n")
+            Intro()
+        elif choice == "2":
+            print("Welcome to the wizard class\n")
+            Player.Pclass = 'wizard'
+            Player.Strength = 5
+            Player.Defense = 3
+            Player.hp = 50
+            Player.MaxHP = 50
+            EquipInsert('wand')
+            print("You have been given a wand.")
+            EquipInsert('cloth armor')
+            print("You have been given a cloth armor\n")
+            Intro()
+        elif choice == "3":
+            print("Welcome to the rogue class\n")
+            Player.Pclass = 'rouge'
+            Player.Strength = 7
+            Player.Defense = 5
+            Player.hp = 70
+            Player.MaxHP = 70
+            EquipInsert('dagger')
+            print("You have been given a dagger.")
+            EquipInsert('hard cloth armor')
+            print("You have been given a hard cloth armor.\n")
+            Intro()
+        else:
+            print("Wrong choice")
+            GameIntro()
+    elif Player.Pclass != "\"\"\n":
         Intro()
     else:
-        print("Wrong choice")
-        GameIntro()
+        print("GameIntro went wrong!!")
 
 #Gives the player a potion to survive and an awsome introduction to the game
 def Intro():
@@ -266,7 +271,7 @@ def MonsterList():
 
     if tempLvl <= 0:
         tempLvl = 1
-        
+
     for key, value in list(Monsters.items()):
         if Player.lvl == 100:
             if value[Monster.lvl] <= Player.lvl:
@@ -287,14 +292,14 @@ def fight():
 
     while Player.hp > 0 and value[Monster.HP] > 0:
         Pdamage = random.randrange(Player.Strength) - (random.randrange(value[Monster.defense]) + value[Monster.lvl])
-        Edamage = (random.randrange(value[Monster.attack]) + value[Monster.lvl]) - (random.randrange(Player.Defense) + (Player.lvl * 2)) 
+        Edamage = (random.randrange(value[Monster.attack]) + value[Monster.lvl]) - (random.randrange(Player.Defense) + (Player.lvl * 2))
 
         if Pdamage < 0:
             Pdamage = 0
 
         if Edamage < 0:
             Edamage = 0
-    
+
 
         print("You hit the " + key[0] + " for " + str(Pdamage) + " damage")
         value[Monster.HP] -= Pdamage
@@ -612,7 +617,7 @@ def Sell():
 def PrintInven():
     for key, value in list(Player.Inventory.items()):
         print(key + " X " + str(value[Item.count]))
-        
+
 #The store for the player
 def Store():
     print("1.Buy \n2.Sell \n3.Exit")
@@ -707,6 +712,18 @@ def EquipTypeCheck(Etype):
 
 #Exiting the game
 def Exit():
+    with open('PlayerFile.txt', 'w') as f:
+        write_data = f.write(Player.name)
+        write_data = f.write(str(Player.MaxHP) + "\n")
+        write_data = f.write(str(Player.hp) + "\n")
+        write_data = f.write(str(Player.Strength) + "\n")
+        write_data = f.write(str(Player.Defense) + "\n")
+        write_data = f.write(str(Player.gold) + "\n")
+        write_data = f.write(str(Player.MaxExp) + "\n")
+        write_data = f.write(str(Player.exp) + "\n")
+        write_data = f.write(str(Player.lvl) + "\n")
+        write_data = f.write(Player.Pclass)
+
     return True
 
 def DeathCheck():
@@ -741,6 +758,28 @@ def Choices():
 
 #Main game function
 def main():
+
+    with open('PlayerFile.txt') as f:
+        Player.name = f.readline()
+        Player.MaxHP = f.readline()
+        Player.hp = f.readline()
+        Player.Strength = f.readline()
+        Player.Defense = f.readline()
+        Player.gold = f.readline()
+        Player.MaxExp = f.readline()
+        Player.exp = f.readline()
+        Player.lvl = f.readline()
+        Player.Pclass = f.readline()
+
+    Player.MaxHP = int(Player.MaxHP)
+    Player.hp = int(Player.hp)
+    Player.Strength = int(Player.Strength)
+    Player.Defense = int(Player.Defense)
+    Player.gold = int(Player.gold)
+    Player.MaxExp = int(Player.MaxExp)
+    Player.exp = int(Player.exp)
+    Player.lvl = int(Player.lvl)
+
     GameIntro()
     Choices()
 
